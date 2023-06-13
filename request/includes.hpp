6212c36b8b995faae438d511ef253854575eaf17
Hafid Selbi <hselbi@e1r12p7.1337.ma>
2023-06-13 20:58:11 +0100

    hello

--- request/includes.hpp
+++ request/includes.hpp
@@ -1,6 +1,16 @@
 #ifndef INCLUDES_HPP
 #define INCLUDES_HPP
 
+
+#define RESET   "\033[0m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN    "\033[36m"
+#define WHITE   "\033[37m"
+
 #include <string>
 #include <vector>
 #include <iostream>
@@ -8,10 +18,15 @@
 #include <map>
 #include <list>
 #include <sstream>
+# include <utility>
 #include <ctime>
 #include <dirent.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <algorithm>
+#include <cctype>
+#include <iomanip>
+ 
 #include <arpa/inet.h>
 
 #include <sys/types.h>
