5d1ad86f8070056baf4ce28f4739807d95ce6313
Hafid Selbi <hselbi@e1r10p5.1337.ma>
2023-06-16 19:03:01 +0100

    h

--- config/main.cpp
+++ config/main.cpp
@@ -12,11 +12,11 @@ int main(int argc, char **argv)
 
 	std::string config = (argc == 1) ? "./conf/test.config" : argv[1];
 	Config configParser(config.c_str());
-	std::vector<ConfServer> *servers = configParser.parser();
-	std::cout << "this is size of servers: " << servers->size() << std::endl;
+	std::vector<ConfServer> servers = configParser.parser();
+	std::cout << "this is size of servers: " << servers.size() << std::endl;
 	// std::cout << "====================" << i << "====================\n";
-	std::vector<ConfServer>::iterator it = servers[0].begin();
-	std::vector<ConfServer>::iterator ite = servers[0].end();
+	std::vector<ConfServer>::iterator it = servers.begin();
+	std::vector<ConfServer>::iterator ite = servers.end();
 	int i = 0;
 	for (; it != ite; it++)
 	{
