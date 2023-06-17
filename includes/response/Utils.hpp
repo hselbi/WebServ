9a71428fdae1a11e44107717013e76c3e02d3875
ZakariaMahmoud <zakaria.forwork@gmail.com>
2023-06-17 18:26:20 +0100

    Add directory includes

---
+++ includes/response/Utils.hpp
@@ -0,0 +1,31 @@
+#ifndef UTILS_HPP
+# define UTILS_HPP
+#include <string>
+#include <iostream>
+#include <cstring>
+#include <fstream>
+#include <unistd.h>
+#include <sstream>
+#include <map>
+#include <vector>
+#include <dirent.h>
+
+
+typedef struct ResponseHeader {
+    int statusCode;
+    std::string statusMessage;
+    std::map<std::string, std::string> headers;
+} t_responseHeader;
+
+
+class Utils
+{
+	public:
+		static std::string toString(int value);
+		static std::string ResponseHeaderToString(const t_responseHeader &responseHeader);
+		static bool isValidURI(const std::string& uri);
+		static bool isDirectory(const std::string& path);
+		static bool fileExists(const std::string& path);
+};
+
+#endif
