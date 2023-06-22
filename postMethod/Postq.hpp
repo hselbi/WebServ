c6223c4af848870d01e858c4af0d1e97f559a00e
slo <selbihafid@gmail.com>
2023-06-22 22:27:51 +0100

    hafid

---
+++ postMethod/Postq.hpp
@@ -0,0 +1,19 @@
+#ifndef POST_HPP
+#define POST_HPP
+
+// #include "Request.hpp"
+
+class Post
+{
+private:
+    bool _chunked;
+    bool _length;
+    bool _type;
+public:
+    Post(Request &req);
+    ~Post();
+};
+
+
+
+#endif
