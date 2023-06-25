e8de2d0f60509446d013cb1d816767d46863fa3f
sbb3 <anasdouib2021@outlook.com>
2023-06-25 11:03:43 +0100

    ++

--- core/srcs/Server.cpp
+++ core/srcs/Server.cpp
@@ -220,15 +220,15 @@ bool Server::is_request_completed(std::string &request, long client_socket)
 
 void Server::match_client_request_to_server_block(long client_socket)
 {
-	// for (std::vector<ConfServer>::iterator server_block = _server_blocks->begin(); server_block != _server_blocks->end(); ++server_block)
-	// {
-	// 	// !! getHost still not implemented
-	// 	if (server_block->getHost() == get_client(client_socket)->get_request().getHeaders()["Host"] && server_block->getPort() == get_client(client_socket)->get_request().getPort())
-	// 	{
-	// 		get_client(client_socket)->set_server_block(*server_block);
-	// 		return;
-	// 	}
-	// }
+	for (std::vector<ConfServer>::iterator server_block = _server_blocks.begin(); server_block != _server_blocks.end(); ++server_block)
+	{
+		// // !! getHost still not implemented
+		// if (server_block->getHost() == get_client(client_socket)->get_request().getHeaders()["Host"] && server_block->getPort() == get_client(client_socket)->get_request().getPort())
+		// {
+		// 	get_client(client_socket)->set_server_block(*server_block);
+		// 	return;
+		// }
+	}
 }
 
 void Server::handle_incoming_request(long client_socket) // ready to read socket descriptor
@@ -401,7 +401,7 @@ void Server::setup_server()
 	zero_socket_pool();
 	long server_socket;
 	// for (long i = 0; i < SERVER_BLOCK_COUNT; i++)
-	for (long i = 0; i < _server_blocks->size(); i++)
+	for (long i = 0; i < _server_blocks.size(); i++)
 	{
 		std::cout << "server " << i <<  std::endl;
 		create_server_socket();
