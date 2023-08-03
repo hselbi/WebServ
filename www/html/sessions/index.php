b38371d1052b30cc76690bc26b85c1b158728ce4
Zakaria Mahmoud <zmahmoud@e1r11p12.1337.ma>
2023-08-04 00:12:24 +0100

    New update

--- www/html/sessions/index.php
+++ www/html/sessions/index.php
@@ -1,7 +1,6 @@
+<?php session_start(); ?>
 
-<?php session_start();?>
-
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
